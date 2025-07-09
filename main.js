2c2a6608e476b7709ab3157157bdb1e20b76f72a
Salvatore <s.milone@3em.it>
2025-07-09 13:57:37 +0200

    test

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const rawText = params.get('text');
 
 if (rawText) {
     const formatted = parseFormattedText(rawText);
-    document.getElementById('codeBlock').innerHTML = parseFormattedText(rawText);
+    document.getElementById('codeBlock').innerHTML = formatted;
 } else {
     document.getElementById('codeBlock').textContent = "Errore";
 }
@@ -36,20 +36,19 @@ function shareText() {
 }
 
 function parseFormattedText(raw) {
-  // 1) decodifica URI
   let text = decodeURIComponent(raw);
-  
-  // 2) esegui le tue formattazioni di markdown-like
+
+  // Sostituzioni base
   text = text.replaceAll('__', '\n').replaceAll('_', ' ');
+
+  // Ordine: grassetto → corsivo → sottolineato → titolo
   text = text.replace(/\*(.+?)\*/g, '<strong>$1</strong>');
   text = text.replace(/-(.+?)-/g, '<em>$1</em>');
   text = text.replace(/\$(.+?)\$/g, '<u>$1</u>');
   text = text.replace(/#(.+?)#/g, '<span class="custom-title">$1</span>');
+
+  // A capo visivo
   text = text.replace(/\n/g, '<br>');
-  
-  // 3) sanitizza il risultato
-  return DOMPurify.sanitize(text, {
-    ALLOWED_TAGS: ['strong','em','u','span','br'],
-    ALLOWED_ATTR: ['class']
-  });
+
+  return text;
 }
